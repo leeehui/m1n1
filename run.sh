41fcc6168f900969857b6735968f86c3a13b0853
lihui <leehui_hrbeu71102@163.com>
2021-03-23 20:48:18 +0800

    add more run.sh
    1. more comments in case time washes my memory away
    2. fix "only 1 sub-dir" bug
    3. first elf of gkb sub-item related error, ugly work!!
    
    Signed-off-by: lihui <leehui_hrbeu71102@163.com>

--- run.sh
+++ run.sh
@@ -35,18 +35,26 @@ gen_cmd_file_for_qemu_rolling()
 	warmup_inst_num=`echo $temp | awk -F " " '{print $2 }'`
 	total_inst_num=`echo $temp | awk -F " " '{print $4 }'`
 	
+	# this info will be used by report.py. in short, this will be used to determine whether to ignore
+	# the first rolling line. as report.py also have a mode(mostly for spec related test) ignoring the
+	# first rolling line and we want do all the ugly modes in one script. see report.py for more detail
 	echo "warmup_inst_num=$warmup_inst_num" > $log_file
 	echo "total_inst_num=$total_inst_num" >> $log_file
 	echo "rolling_interval=20000000" >> $log_file
 
 	echo "show_elf" > $cmd_file
+	echo "change_config rolling_interval 20000000" >> $cmd_file
+
+	# for the first elf of every gkb sub-item, -w is fixed to 0,
+	# and is gaurranteed to have svc inst, use unpack rolling mode
 	if [ "0" = $warmup_inst_num ]; then
 		echo "run_elf_unpack 0 0 2" >> $cmd_file
 		echo "run_elf_unpack 0 0 2" >> $cmd_file
 		echo "4 run_elf_unpack 0 0 2" >> $cmd_file
 		echo "4 run_elf_unpack 0 0 2" >> $cmd_file
+	# for others that -w is not 0, use -w to skip the warmup
+	# also before this, we have write -w -i relate info used for report.py
 	else
-		echo "change_config rolling_interval 20000000" >> $cmd_file
 		echo "run_elf_qemu 0 0 $warmup_inst_num 2" >> $cmd_file
 		echo "run_elf_qemu 0 0 $warmup_inst_num 2" >> $cmd_file
 		echo "4 run_elf_qemu 0 0 $warmup_inst_num 2" >> $cmd_file
@@ -68,6 +76,10 @@ handle_path()
 		use_ca_list=on
 	fi
 	test_output_dir=$test_dir/output
+	
+	# cheap but maybe not elegant way to fix the following file_dirs related Bug
+	# force mkdir a-fake-dir to make the test dir have at least 2 sub-dir
+	mkdir -p $test_dir/a-fake-dir
 	
 	cd $test_dir
 
